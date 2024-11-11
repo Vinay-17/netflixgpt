696a9ea2251c1ec52b058b0ed64a8d835c2e60e4
Vinay-17 <vinayveerapaneni068@gmail.com>
2024-11-11 21:13:42 +0530

    Initial commit

---
+++ OneDrive/Desktop/NETFLIXGPT/netflix/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className="text-5xl font-bold underline text-green-700">
+      NETFLIX-GPT
+    </h1>
+    </>
+  )
+}
+
+export default App
